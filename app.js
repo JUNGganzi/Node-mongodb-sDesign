78be13ae2ef5f1a00bbed4c89eb805b9ac0636c1
JUNGganzi <wjdwjd1501@gmail.com>
2021-01-21 23:00:51 +0900

    swagger ui 띄우기 완료

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express'); // rest api 필수템
-const mongoose = require('mongoose'); // mongodb 연동 필수템 
-const bodyparser = require('body-parser');
-const app = express();
-const User = require('./models/User');
+var express = require('express'); // rest api 필수템
+var mongoose = require('mongoose'); // mongodb 연동 필수템
+var swaggerJsdoc = require('swagger-jsdoc');
+var swaggerUi = require('swagger-ui-express'); 
+var bodyparser = require('body-parser');
+var app = express();
+var User = require('./models/User');
+const swaggerJSDoc = require('swagger-jsdoc');
 require('dotenv').config({path:'variables.env'}) // variables.env 에 mongoDB 관련 비밀번호와 URL 노출방지를 위해 .env 파일생성 및 dotenv 설치 및 연동
 
 
@@ -16,6 +19,25 @@ app.listen(3000,function(){
     console.log('서버가동중')
 });
 
+// swagger 변수설정
+const options = {
+    definition: {
+        openapi : '3.0.0',
+        info : {
+            title : 'sDesign API 문서',
+            version : '1.0',
+            // description : '냉무',
+        },
+        host : 'localhost:3000',
+        basepath : '/',
+    },
+    apis : [],
+};
+
+const specs = swaggerJsdoc(options);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// mongoose 연결
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err) {  // process.env.MONGODB_URL 만든 .env 파일안에 MONGODB_URL 전달
     if(err) {                             // {useNewUrlParser: true, useUnifiedTopology: true} 없으면 에러뜬다 69,74번 번갈아보면서확인
         console.log(err);
@@ -24,21 +46,21 @@ mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopo
     }
 });
 
-app.get('/', function(req, res){
-    let newUser = new User();
-    newUser.email = 'wjdwjd1501@gmail.com';
-    newUser.name = 'wjdwjddnjs';
-    newUser.password = '12341234';
-    newUser.save()
-        .then(function(user){
-            console.log(user);
-            res.json({
-                message:'User 생성완료'
-            })
-        })
-        .catch(function(err){
-            res.json({
-                message : 'User 생성불가!'
-            })
-        })
-});
+// app.get('/', function(req, res){
+//     let newUser = new User();
+//     newUser.email = 'wjdwjd1501@gmail.com';
+//     newUser.name = 'wjdwjddnjs';
+//     newUser.password = '12341234';
+//     newUser.save()
+//         .then(function(user){
+//             console.log(user);
+//             res.json({
+//                 message:'User 생성완료'
+//             })
+//         })
+//         .catch(function(err){
+//             res.json({
+//                 message : 'User 생성불가!'
+//             })
+//         })
+// });
