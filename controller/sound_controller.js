c40f20d587afbdb5a752802621d8dd33b3091d19
JUNGganzi <wjdwjd1501@gmail.com>
2021-02-03 22:42:12 +0900

    file upload setting

--- controller/sound_controller.js
+++ controller/sound_controller.js
@@ -1,5 +1,6 @@
 const Sound = require('../models/sound_model');
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
 require('dotenv').config();
 
 const MY_SECRET_KEY = process.env.SECRET_KEY
