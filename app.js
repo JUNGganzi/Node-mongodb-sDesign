851b2cd2f042ae95c94a8f74d214cfc7b17cb2d6
JUNGganzi <wjdwjd1501@gmail.com>
2021-03-15 00:27:21 +0900

    file limit 재설정

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ require('dotenv').config({path:'variables.env'})
 
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit : "50mb"}));
+app.use(bodyParser.urlencoded({ extended: false, limit:'1mb' }));
 
 app.use(cors());
 
